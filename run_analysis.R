fcb4ace77b153b5a028d8509931a02c1269ba7d5
Jason Simmons <celetavaro@gmail.com>
2017-11-21 21:12:24 -0700

    Added commented rough draft or script functionality

---
+++ run_analysis.R
@@ -0,0 +1,11 @@
+# Part I:  read in data sets and merge them
+
+
+# Part II:  Extract measurements only on the mean and std of each measurement
+
+# Part III:  Descriptive Activity Names for activities in dataset
+
+# Part IV:  Dataset should have appropriate/descriptive variable names
+
+# Part V:  From the data set in step 4, creates a second, independent tidy data set 
+#   with the average of each variable for *each activity* and *each subject*.
