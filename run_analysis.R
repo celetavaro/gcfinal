2c9e1cead546bdb778251ebdf9c9dfbeb7287aff
Jason Simmons <celetavaro@gmail.com>
2017-12-01 22:14:42 -0700

    added and tested(in console) reading in of data

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,20 @@
 # Part I:  read in data sets and merge them
 
+xtest <- read.table("./data/test/X_test.txt")
+
+ytest <- read.table("./data/test/y_test.txt")
+
+stest <- read.table("./data/test/subject_test.txt")
+
+xtrain <- read.table("./data/train/X_train.txt")
+
+ytrain <- read.table("./data/train/y_train.txt")
+
+strain <- read.table("./data/train/subject_train.txt")
+
+# NOTE:  change names from V1 to descriptive before cbinidning 
+  # all have first col. vectors loaded as "V1"
+
 
 # Part II:  Extract measurements only on the mean and std of each measurement
 
