8eecb2d37615b88f4f8606078d18c82cdde8ea1c
Jason Simmons <celetavaro@gmail.com>
2017-12-03 12:44:35 -0700

    functionality changes
    added filecheck/download functionality, changed file paths to reflect new changes.  Streamlined cbind, rbind into one call.  Formatting.

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,20 @@
-# URL for data - add download/check/unzip, etc.
-# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-# afer unzip: "UCI HAR Dataset" with subdirs /test and /train
+# check for data, download & unzip if not present
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  filename <- "getdata_projectfiles_UCI HAR Dataset.zip"
+    datadir <- "UCI HAR Dataset"
 
-
-# read in features file and use this to name the columns features[,2] will have the names
-
-
+    if(!file.exists(datadir)){
+        download.file(fileURL, filename, method = "curl")
+        unzip(filename)
+    }
 # read in data sets 
-xtest <- read.table("./data/test/X_test.txt")
-  ytest <- read.table("./data/test/y_test.txt")
-    stest <- read.table("./data/test/subject_test.txt")
+xtest <- read.table( file.path(datadir, "test", "X_test.txt"))
+  ytest <- read.table( file.path(datadir,"test","y_test.txt"))
+    stest <- read.table( file.path(datadir, "test", "subject_test.txt"))
     
-xtrain <- read.table("./data/train/X_train.txt")
-  ytrain <- read.table("./data/train/y_train.txt")
-    strain <- read.table("./data/train/subject_train.txt")
+xtrain <- read.table( file.path(datadir, "train", "X_train.txt"))
+  ytrain <- read.table( file.path(datadir, "train", "y_train.txt"))
+    strain <- read.table( file.path(datadir, "train", "subject_train.txt"))
 
 # assign names before merge to avoid having multiple "V1" columns
 names(stest) <- "subjectID"
@@ -23,30 +24,27 @@ names(stest) <- "subjectID"
       names(ytrain) <- "activity"
   
 # merge test set
-testdata <- cbind(ytest, xtest)
-  testdata <- cbind(stest, testdata)
-
+testdata <- cbind(stest, ytest, xtest)
     # merge training set
-    traindata <- cbind(ytrain, xtrain)
-      traindata <- cbind(strain, traindata)
+    traindata <- cbind(strain, ytrain, xtrain)
   
 # merge both sets into one big set. 
 dset <- rbind(testdata, traindata)
-
     # to get the subjectID's in order
     dset <- dset[order(dset$subjectID), ]
 
 
 # Part II:  Extract measurements only on the mean and std of each measurement
-    
     # use grep/grepl to pick the cols with[Mm]ean or [Ss]td
-
-    # Descriptive Activity Names for activities in dataset
     
-    lookup <- c("walking", "upstairs", "downstairs", "sitting", "standing", "laying")
-    dset$activity <- lookup[dset$activity]
+# Descriptive Activity Names for activities in dataset
+lookup <- c("walking", "upstairs", "downstairs", "sitting", "standing", "laying")
+dset$activity <- lookup[dset$activity]
 
 # Part IV:  Dataset should have appropriate/descriptive variable names
+  # TODO:  read in features file and use this to name the columns 
+  # features[,2] will have the names
+
 
 # Part V:  From the data set in step 4, creates a second, independent tidy data set 
 #   with the average of each variable for *each activity* and *each subject*.
